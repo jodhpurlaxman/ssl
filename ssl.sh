b10d1be0213d3f25db6fd08c4756fa899b9064ca
Laxman Singh <jodhpur.laxman@gmail.com>
2020-10-22 15:16:03 +0530

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -6,10 +6,8 @@ if [ `whoami` != 'root' ]
     exit
 fi
 
-echo "Enter Your doamin name without www"
-read domainname
-echo "Enter Your Site Web Root path like /home/$somainname/public_html"
-read webrootis
+read -p "Enter Your doamin name without www:" domainname
+read -p "Enter Your Site Web Root path like /home/$somainname/public_html" webrootis
 echo $domainname
 echo $webrootis
 #/root/.acme.sh/acme.sh --issue -d $domainname --cert-file /etc/letsencrypt/live/$domainname/cert.pem --key-file /etc/letsencrypt/live/$domainname/privkey.pem --fullchain-file /etc/letsencrypt/live/$domainname/fullchain.pem -w  $webrootis --force >> /home/cyberpanel/error-logs.txt && /usr/local/lsws/bin/lswsctrl restart 
